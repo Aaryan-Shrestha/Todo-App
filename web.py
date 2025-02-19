6806cce3122a56a23f8152a9f9e3fa0247b8a127
Aaryan <downloadsoft17@gmail.com>
2025-02-19 10:59:11 +0545

    Implementing "Add New Todo" feature in WGUI

--- web.py
+++ web.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -10,4 +17,11 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="Enter todos: ",
+              label_visibility = "hidden",
+              placeholder="Add new todo...",
+              on_change=add_todo,
+              key="new_todo")
+
+
+st.session_state # Dictionary
